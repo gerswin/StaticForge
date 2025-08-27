ba11c850b48ef6bf8670cf5e8ff0e7786f7a5735
gerswin <g3rswin@gmail.com>
2025-08-26 20:54:59 -0500

    chore(release): v1.3.2\n\n- Slugs jerárquicos soportados (e.g., /seguros/arl/)\n- Actualiza header y changelog

--- static-page-generator.php
+++ static-page-generator.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: StaticForge
  * Description: Convierte WP en HTML sólido
- * Version: 1.3.1
+ * Version: 1.3.2
  * Author: Gerswin Pineda
  * Update URI: https://github.com/gerswin/StaticForge
  * Requires at least: 6.0
